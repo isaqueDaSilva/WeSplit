72fb677c46f6bb60522dd206947a80de290fea68
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-14 15:59:09 -0300

    WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Isaque da Silva on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
