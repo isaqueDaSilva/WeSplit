5f6eb1d6286eaf5c164905a7153104a1e785f0b5
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-15 21:23:15 -0300

    WeSplit 1.1

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+extension Double {
+    func percentage() -> Int {
+        var percentage = self * 100
+        return Int(percentage)
+    }
+}
+
 struct ContentView: View {
+    @State private var checkAmount: Double = 0
+    @State private var numberOfPeoples: Int = 2
+    @State private var tipPercentage: Double = 0.2
+    
+    let percentages = [0, 0.10, 0.15, 0.20, 0.25]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Aumont", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    
+                    TextField("Numbers of people", value: $numberOfPeoples, format: .number)
+                    
+                    Picker("Select tip percentage", selection: $tipPercentage) {
+                        ForEach(percentages, id: \.self) { percentage in
+                            Text("\(percentage.percentage())%")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
